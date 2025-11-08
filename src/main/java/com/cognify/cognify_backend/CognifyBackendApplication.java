47a160126da75f63e1ac0ebc85728619ee1a7fc7
Dharshan S Kotian <dcodecraft@gmail.com>
2025-11-08 22:11:48 +0530

    initial commit

---
+++ src/main/java/com/cognify/cognify_backend/CognifyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cognify.cognify_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognifyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cognify/cognify_backend/CognifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+// package com.cognify.cognify_backend;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class CognifyBackendApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
